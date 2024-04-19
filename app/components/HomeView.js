804c5b0e1684f578d16c17a73d89d3ce003e82e9
Simon Jern <simonnjern@gmail.com>
2024-04-19 13:40:41 +0200

    Added weather API
    ...and a few preparing tweaks for displaying the results.

--- app/components/HomeView.js
+++ app/components/HomeView.js
@@ -1,55 +1,73 @@
+"use client";
+
 import { Box, Sheet, SvgIcon, Typography } from "@mui/joy";
 
-export default function HomeView({ imageUrl }) {
+export default function HomeView({ imageUrl, weatherData }) {
+  console.log(weatherData);
   return (
-    <Box
-      width="100vw"
-      height="100vh"
-      sx={{
-        backgroundImage: `url(${imageUrl})`,
-        backgroundSize: "cover",
-        position: "absolute",
-      }}
-    >
-      <Sheet
-        sx={{ marginTop: "20px", maxWidth: "fit-content", padding: "20px" }}
-        variant="solid"
-        color="success"
-      >
-        <Typography sx={{ color: "#FFF" }} level="h1">
-          WeatherApp
-        </Typography>
-      </Sheet>
-      <Sheet
+    <Box height="100vh" display="flex" flexDirection="column">
+      <Box
+        width="100vw"
+        minHeight="400px"
         sx={{
-          marginTop: "20px",
-          maxWidth: "fit-content",
-          padding: "20px",
-          float: "right",
+          flexBasis: { xs: "30%", md: "70%" },
+          backgroundImage: `url(${imageUrl})`,
+          backgroundSize: "cover",
         }}
-        variant="solid"
-        color="success"
       >
-        <Typography sx={{ color: "#FFF" }} level="h1">
-          <SvgIcon size="lg">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-              />
-            </svg>
-          </SvgIcon>{" "}
-          Malmö
-        </Typography>
-      </Sheet>
+        <Sheet
+          sx={{ marginTop: "20px", maxWidth: "fit-content", padding: "20px" }}
+          variant="solid"
+          color="success"
+        >
+          <Typography sx={{ color: "#FFF" }} level="h1">
+            WeatherApp
+          </Typography>
+        </Sheet>
+        <Sheet
+          sx={{
+            marginTop: "20px",
+            maxWidth: "fit-content",
+            padding: "20px",
+            float: "right",
+          }}
+          variant="solid"
+          color="success"
+        >
+          <Typography sx={{ color: "#FFF" }} level="h1">
+            <SvgIcon size="lg">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                />
+              </svg>
+            </SvgIcon>{" "}
+            Malmö
+          </Typography>
+        </Sheet>
+      </Box>
+      <Box
+        sx={{
+          flexBasis: { xs: "70%", md: "30%" },
+          width: "100%",
+          bottom: 0,
+          background: "rgba(31, 122, 31, 0.2)",
+          boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+          backdropFilter: "blur(5px)",
+          border: "1px solid rgba(255, 255, 255, 0.3)",
+        }}
+      >
+        Test
+      </Box>
     </Box>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,12 +1,21 @@
 import HomeView from "./components/HomeView";
 
-export default function Home() {
-  
+const getWeatherData = async (city) => {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=3226c53da11f4dc996f142724231809&q=${city}&days=5&aqi=no&alerts=no`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch weather data");
+  }
+
+  return response.json();
+};
+
+export default async function Home() {
+  const city = "Malmö"; //Read from cookies later
+  const weatherData = await getWeatherData(city);
   return (
-    <HomeView
-      imageUrl={
-        "https://images.unsplash.com/photo-1569764102256-f7ff0673bd05?q=80&w=1933&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      }
-    />
+    <HomeView imageUrl={backgroundImage.urls.full} weatherData={weatherData} />
   );
 }
